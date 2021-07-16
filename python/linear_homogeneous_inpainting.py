1124cf71054cfdf4df772093bbf5eeb2c21ed520
anirban.ghosh <anirban.ghosh@scai.fraunhofer.de>
2021-07-16 14:37:14 +0200

    some try

--- python/linear_homogeneous_inpainting.py
+++ python/linear_homogeneous_inpainting.py
@@ -6,51 +6,39 @@ from datetime import datetime
 from scipy import ndimage, signal
 
 
-def derivative_xx(image):
-    width, height = image.shape[0], image.shape[1]
+def derivative_xx(image, width, height):
+    #width, height = image.shape[0], image.shape[1]
     len = width * height
-    dxx = np.identity(width)
-    for d in range(0, width - 1):
-        dlen = d * width * height;
-        slen = width * height;
+    dxx = np.ones(len)
 
-        # print ( "dlen ", dlen )
+    # out_array
+    for i in range(1, width - 1):
+        # print (" i  ", i, "  i+1+dlen  ", i+1+dlen, " i+dlen " , i+dlen, "  i-1+dlen  ", i-1+dlen )
+        dxx[i] = (image[i + 1] - 2 * image[i] + image[i - 1])
 
-        # print ("slen  : " ,slen)
+    for k in range(0, height):
+        # print(" k * width+dlen  ", k * width+dlen, "  k * width+1+dlen ", \
+        #             k * width+1+dlen, " k * width+dlen ", k * width+dlen)
+        dxx[k * width] = (image[k * width + 1] - image[k * width])
 
-        # print("slen - width : " , slen - width)
+        # print("(k+1) * width-1+dlen  ", (k+1) * width-1+dlen, " (k+1) * width-2+dlen ", \
+        #           (k+1) * width-2+dlen, "(k+1) * width-1+dlen ", (k+1) * width-1+dlen)
+        dxx[(k + 1) * width - 1] = (image[(k + 1) * width - 2] - image[(k + 1) * width - 1])
 
-        for i in range(1, height - 1):
-            # print (" i  ", i, "  i+1+dlen  ", i+1+dlen, " i+dlen " , i+dlen, "  i-1+dlen  ", i-1+dlen )
-            dxx[i] = (image[i - 1] - 2 * image[i] + image[i + 1])
-
-        for k in range(0, height):
-            print(" k * width+dlen  ", k * width+dlen, "  k * width+1+dlen ", \
-                  k * width+1+dlen, " k * width+dlen ", k * width+dlen)
-            dxx[k * width+dlen] = (image[k*width+1+dlen]-image[k * width+dlen])
-
-            print("(k+1) * width-1+dlen  ", (k+1) * width-1+dlen, " (k+1) * width-2+dlen ", \
-                  (k+1) * width-2+dlen, "(k+1) * width-1+dlen ", (k+1) * width-1+dlen)
-            dxx[(k+1) * width-1+dlen] = (image[(k+1) * width-2+dlen]-image[(k+1) * width-1+dlen])
-
-            print("-----------------------------------------------")
+        #print("-----------------------------------------------")
     return dxx
 
 
-def derivative_yy(image):
-    width, height = image.shape[0], image.shape[1]
+def derivative_yy(image, width, height):
+    #width, height = image.shape[0], image.shape[1]
     len = width * height
-    dyy = np.identity(width)
-    for d in range(0, height - 1):
-        dlen = d * width * height;
-        slen = width * height;
+    dyy = np.ones(len)
+    for i in range(width, len - width):
+        dyy[i] = (image[1 + width] - 2 * image[i] + image[1 - width])
 
-        for i in range(1, width - 1):
-            dyy[i] = (image[i - 1] - 2 * image[i] + image[i + 1])
-
-        for k in range (0,height-1):
-            dyy[k * height+dlen] = (image[k * height+1+dlen]-image[k * height+dlen])
-            dyy[(k+1) * height-1+dlen] = (height[(k+1) * height-2+dlen]-image[(k+1) * height-1+dlen])
+    for k in range(0, width):
+        dyy[k] = (image[k + width] - image[k])
+        dyy[k + width * (height - 1)] = (image[(height - 2) * width + k] - image[k + width * (height - 1)])
     return dyy
 
 
@@ -72,7 +60,7 @@ def display_image(image):
 def linear_homogeneous_FSI():
     currentTime = datetime.now()
 
-    num_of_steps = 1
+    num_of_steps = 10
     threshold = 0.2
     image = read_image('pepper.png')
     mask_image = read_image('pepper_15percentage_of_random_pixels.png')
@@ -83,41 +71,63 @@ def linear_homogeneous_FSI():
     finalImage = image - mask_image
     width, height = finalImage.shape[0], finalImage.shape[1]
     print(finalImage.shape)
+
+    #converting to 1d
+    original_width, original_height = image.shape[0], image.shape[1]
+    masked_width, masked_height =  finalImage.shape[0], finalImage.shape[1]
+
+    image_1d = image.reshape(original_width*original_height)
+    maskedImage_1d = finalImage.reshape(masked_width*masked_height)
+
     l2_norm = np.linalg.norm(image - finalImage)
     print("L2 norm = ", l2_norm)
-    presentImage = finalImage
-    prevImage = np.identity(width)
-    currentImage = finalImage
 
-    #while l2_norm > dxx:
+    presentImage = maskedImage_1d
+    prevImage = maskedImage_1d
+    currentImage = maskedImage_1d
+
+
+    # while l2_norm > dxx:
     for n in range(0, num_of_steps):
-        dxx = ndimage.correlate1d(presentImage, [1, -2, 1], axis=0)  # derivative_xx(finalImage)
-        dyy = ndimage.correlate1d(currentImage, [1, -2, 1], axis=1)  # derivative_yy(finalImage)
+        dxx = derivative_xx(maskedImage_1d, masked_width, masked_height)  # ndimage.correlate1d(presentImage, [1, -2, 1], axis=0)
+        dyy = derivative_yy(maskedImage_1d, masked_width, masked_height)  # ndimage.correlate1d(currentImage, [1, -2, 1], axis=1)
         # print("calculated dxx and dyy")
-        #display_image(dxx)
-        #display_image(dyy)
-        #dImage = np.hypot(dxx, dyy)
-        dImage = (dxx+dyy)
-        #print("Dimages  = ", ((dImageOld - dImage) ** 2).mean())
-        #display_image(dImage)
+        # display_image(dxx)
+        # display_image(dyy)
+        # dImage = np.hypot(dxx, dyy)
+        dImage = (dxx + dyy)
+        # print("Dimages  = ", ((dImageOld - dImage) ** 2).mean())
+        # display_image(dImage)
         alpha = (4 * n + 2) / (2 * n + 3)
-        currentImage = presentImage
         traceIndex = 0
-#        for i in range (0 ,width):
-#         if n == 0:
-#             prevImage = presentImage
-        presentImage = alpha * (presentImage + (timeStepSize * (dImage))) + (1 - alpha) * prevImage
-        prevImage = currentImage
+        #print(masked_width*masked_height)
+        for i in range (0, masked_width*masked_height-2):
+            if n == 0:
+                prevImage[i] = presentImage[i]
 
-    l2_norm = np.linalg.norm(image - presentImage)
+            currentImage[i] = presentImage[i]
+
+            if (i == pixel_locations[traceIndex]):
+                #print("here at ", traceIndex)
+                traceIndex+=1
+                #print(traceIndex)
+                continue
+
+            presentImage[i] = alpha * (presentImage[i] + (timeStepSize * (dxx[i]+dyy[i]))) + (1 - alpha) * prevImage[i]
+            prevImage[i] = currentImage[i]
+
+    l2_norm = np.linalg.norm(image_1d - presentImage)
     print("L2 norm = ", l2_norm)
 
-    mse = ((image - presentImage) ** 2).mean()
+    mse = ((image_1d - presentImage) ** 2).mean()
     print("Mean Squared Error  = ", mse)
 
+    print_image = presentImage.reshape(original_width,original_height)
+
     print("Run Time = ", datetime.now() - currentTime)
-    display_image(presentImage)
+    display_image(print_image)
     return 0
+
 
 def EED_FSI():
     num_of_steps = 5
@@ -128,7 +138,7 @@ def EED_FSI():
     finalImage = image - mask_image
     presentImage = finalImage
 
-    t = np.linspace(-10,10,30)
+    t = np.linspace(-10, 10, 30)
     bump = np.exp(-0.1 * t ** 2)
     bump /= np.trapz(bump)  # normalize the integral to 1
     # make a 2-D kernel out of it
@@ -161,8 +171,6 @@ def EED_FSI():
         #             prevImage = presentImage
         presentImage = alpha * (presentImage + (timeStepSize * (dImage))) + (1 - alpha) * prevImage
         prevImage = currentImage
-
-
 
     print("Run Time = ", datetime.now() - currentTime)
     display_image(presentImage)
